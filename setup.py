865ada6c0db09baf6d031d35585212d6f2a852b0
Brett Weir <brett@lamestation.com>
2015-02-24 01:43:12 -0800

    Add auto versioning so no more bump rev...

--- setup.py
+++ setup.py
@@ -3,9 +3,12 @@ from setuptools import setup, find_packages
 with open('README.rst') as f:
         long_description = f.read()
 
+import subprocess
+version = subprocess.check_output(['git','describe','--tags']).strip()
+
 setup(
         name = "packthing",
-        version = "0.1.3",
+        version = version,
         author = "LameStation",
         author_email = "contact@lamestation.com",
         description = "Write once, package everywhere",
