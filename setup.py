67575e72ac99daafc3ef6ce56fe57572c5e05b50
Brett Weir <brett@lamestation.com>
2015-04-28 23:09:06 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as f:
 
 setup(
         name = "packthing",
-        version = '0.1.7',
+        version = '0.2.1',
         author = "LameStation",
         author_email = "contact@lamestation.com",
         description = "Write once, package everywhere",
