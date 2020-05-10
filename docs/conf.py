ff6e99b31e1b06ced1455b254318936f3843a827
Brett Weir <brett@lamestation.com>
2020-05-09 22:53:16 -0700

    Run black / isort

--- docs/conf.py
+++ docs/conf.py
@@ -1,8 +1,10 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
 
-from packthing import __version__
+sys.path.insert(0, os.path.abspath(".."))
+
+from packthing import __version__  # isort:skip
+
 
 project = "packthing"
 copyright = "2020, LameStation"
